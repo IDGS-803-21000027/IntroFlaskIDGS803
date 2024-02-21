47b5901ae8b07c644d253014c90305429ac3edf9
IDGS-803-21000027 <21000027@alumnos.utleon.edu.mx>
2024-02-20 18:32:40 -0600

    Archivo de configuracion

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+from flask import redirect
+
+from flask import flash
 
 import forms
 
 app = Flask(__name__)
+app.secret_key='esta es la clave secreta'
 
-@app.route("/")
+#--------------------------------------
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.before_request
+def before_request():
+    #g.nombre='Daniel'
+    
+    print('before_request')
+
+@app.after_request
+def after_request(response):
+    print('after request')
+    #if 'Daniel' not in 'datos' and request.endpoint not in ['/index']:
+    #    return redirect('index')
+    return response
+
+#-------------------------------------
+@app.route("/index")
 def index():
+    g.nombre = 'Daniel'
     escuela="UTL!!"
     alumnos=["Mario", "Pedro", "Luis", "Dario"]
     return render_template("index.html", escuela=escuela, alumnos=alumnos)
 
 @app.route("/alumnos",methods=["GET","POST"])
 def alum():
+    print('dentro de alumnos')
+    print('Hola: {}'.format('Daniel'))
     nom=''
     apa=''
     ama=''
@@ -20,6 +48,11 @@ def alum():
         nom=alum_form.nombre.data
         apa=alum_form.apaterno.data
         ama=alum_form.amaterno.data
+
+        #Agregar mensajes a la lista de flash
+        mensaje='bienvenido a la app {}'.format(nom)
+        flash(mensaje)
+
         print("Nombre: {}".format(nom))
         print("Apaterno: {}".format(apa))
         print("Amaterno: {}".format(ama))
